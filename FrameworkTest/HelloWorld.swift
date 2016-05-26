eeeb65cd29e470d8f3aeb42859284bbb00eb8fdb
Yong Ling <yong.ling@safetyculture.io>
2016-05-27 09:21:38 +1000

    Initial Commit

---
+++ FrameworkTest/HelloWorld.swift
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.swift
+//  FrameworkTest
+//
+//  Created by Yong Ling on 27/05/2016.
+//  Copyright © 2016 Yong Ling. All rights reserved.
+//
+
+import Foundation
+
+@objc public class HelloWorld: NSObject {
+    func hello(world: String) -> String {
+        print(world)
+        return "Job done."
+    }
+}
