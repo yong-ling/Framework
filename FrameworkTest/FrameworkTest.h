ce6547ad8b81ee7ef2bfdf4923734b1f30a14432
Yong Ling <yong.ling@safetyculture.io>
2016-05-27 08:58:43 +1000

    Initial Commit

---
+++ FrameworkTest/FrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest.h
+//  FrameworkTest
+//
+//  Created by Yong Ling on 27/05/2016.
+//  Copyright © 2016 Yong Ling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkTest.
+FOUNDATION_EXPORT double FrameworkTestVersionNumber;
+
+//! Project version string for FrameworkTest.
+FOUNDATION_EXPORT const unsigned char FrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest/PublicHeader.h>
+
+
